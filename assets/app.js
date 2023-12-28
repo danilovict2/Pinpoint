be8af3bd2d67f4a8fb466601eaa7364fde8230ab
danilovict2 <danilosindric@gmail.com>
2023-12-28 22:15:47 +0100

    Refactor map loading logic

--- assets/app.js
+++ assets/app.js
@@ -1,24 +1,5 @@
 import './bootstrap.js';
-import './styles/app.css'
+import './styles/app.css';
+import { initialize } from './maps.js';
 
-function initialize() {
-    const fenway = { lat: 42.345573, lng: -71.098326 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      center: fenway,
-      zoom: 14,
-    });
-    const panorama = new google.maps.StreetViewPanorama(
-      document.getElementById("pano"),
-      {
-        position: fenway,
-        pov: {
-          heading: 34,
-          pitch: 10,
-        },
-      },
-    );
-  
-    map.setStreetView(panorama);
-  }
-  
-  window.initialize = initialize;
+google.maps.event.addDomListener(window, 'load', initialize);
---
+++ assets/maps.js
@@ -0,0 +1,10 @@
+export function initialize() {
+    new google.maps.StreetViewPanorama(
+        document.getElementById("street-view"),
+        {
+            position: { lat: 37.86926, lng: -122.254811 },
+            pov: { heading: 165, pitch: 0 },
+            zoom: 1,
+        }
+    );
+}
