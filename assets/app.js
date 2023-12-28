6fc0b03799faa460ac8404b7c3eeff27bd99fb0a
danilovict2 <danilosindric@gmail.com>
2023-12-28 20:20:45 +0100

    Render map

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,24 @@
 import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
 import './styles/app.css'
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+function initialize() {
+    const fenway = { lat: 42.345573, lng: -71.098326 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: fenway,
+      zoom: 14,
+    });
+    const panorama = new google.maps.StreetViewPanorama(
+      document.getElementById("pano"),
+      {
+        position: fenway,
+        pov: {
+          heading: 34,
+          pitch: 10,
+        },
+      },
+    );
+  
+    map.setStreetView(panorama);
+  }
+  
+  window.initialize = initialize;
