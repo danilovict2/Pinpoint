d1eaa8a9948c865b021afe07181883323157078d
danilovict2 <danilosindric@gmail.com>
2024-01-04 22:22:36 +0100

    Prototype game with vue

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,5 @@
 import './bootstrap.js';
 import { registerVueControllerComponents } from '@symfony/ux-vue';
 import './styles/app.css';
-import { initialize } from './maps.js';
-
-google.maps.event.addDomListener(window, 'load', initialize);
 
 registerVueControllerComponents(require.context('./vue/controllers', true, /\.vue$/));
