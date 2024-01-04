b2b1ed8f1afe40b0c3250e7198c7701d96f3330d
danilovict2 <danilosindric@gmail.com>
2024-01-04 19:49:37 +0100

    minor changes

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,5 @@
-import { registerVueControllerComponents } from '@symfony/ux-vue';
 import './bootstrap.js';
 import './styles/app.css';
 import { initialize } from './maps.js';
 
 google.maps.event.addDomListener(window, 'load', initialize);
-
-registerVueControllerComponents();
