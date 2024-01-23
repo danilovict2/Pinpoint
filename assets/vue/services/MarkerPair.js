a1b7e5bc22b1c77606c85541ecf3b5769cd740fd
Danilo <danilosindric@gmail.com>
2024-01-23 23:15:38 +0100

    Create class to encapsulate markers logic

---
+++ assets/vue/services/MarkerPair.js
@@ -0,0 +1,34 @@
+export default class MarkerPair {
+    constructor(startPosition, endPosition) {
+        this._startPosition = startPosition;
+        this._endPosition = endPosition;
+    }
+
+    get startPositionMarker() {
+        return new google.maps.Marker({ position: this._startPosition });
+    }
+
+    get endPositionMarker() {
+        return new google.maps.Marker({ position: this._endPosition });
+    }
+
+    get lineBetweenMarkers() {
+        const lineSymbol = {
+            path: "M 0,-1 0,1",
+            strokeOpacity: 1,
+            scale: 4,
+        };
+
+        return new google.maps.Polyline({
+            path: [this._startPosition, this._endPosition],
+            strokeOpacity: 0,
+            icons: [
+                {
+                    icon: lineSymbol,
+                    offset: "0",
+                    repeat: "20px",
+                },
+            ],
+        });
+    }
+}
