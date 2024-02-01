34eed263496cc2fec99de8deaac1f35eec137099
Danilo <danilosindric@gmail.com>
2024-02-02 00:03:27 +0100

    implement fit bounds

--- assets/vue/components/Map.vue
+++ assets/vue/components/Map.vue
@@ -4,7 +4,7 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
-const { zoom, center, markers, lines } = defineProps({
+const { zoom, center, markers, lines, fitBounds } = defineProps({
     zoom: {
         type: Number,
         default: 1
@@ -20,6 +20,10 @@ const { zoom, center, markers, lines } = defineProps({
     lines: {
         type: Array,
         default: []
+    },
+    fitBounds: {
+        type: Boolean,
+        default: false
     }
 });
 
@@ -41,12 +45,20 @@ onMounted(() => {
 
     map.addListener("click", e => emit('addMarker', e.latLng, map));
 
-    for(let marker of markers) {
+    for (let marker of markers) {
         marker.setMap(map);
     }
 
-    for(let line of lines) {
+    for (let line of lines) {
         line.setMap(map);
+    }
+
+    if (fitBounds) {
+        const bounds = new google.maps.LatLngBounds();
+        for (let marker of markers) {
+            bounds.extend(marker.position);
+        }
+        map.fitBounds(bounds);
     }
 });
 </script>
