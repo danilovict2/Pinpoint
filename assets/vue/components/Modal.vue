e208c99b57f91cb2c68d514ad6bf3a5d5c1e1203
majstorbob212 <stanticdejo@gmail.com>
2024-03-02 22:03:33 +0100

    added modal component, added homepage bg img

---
+++ assets/vue/components/Modal.vue
@@ -0,0 +1,27 @@
+<template>
+    <transition name="modal-animation">
+      <div v-show="modalActive" class="modal">
+        <transition name="modal-animation-inner">
+          <div v-show="modalActive" class="modal-inner">
+            <i @click="close" class="far fa-times-circle"></i>
+            <!-- Modal Content -->
+            <slot />
+            <button @click="close" type="button">Close</button>
+          </div>
+        </transition>
+      </div>
+    </transition>
+  </template>
+  
+  <script>
+  export default {
+    props: ["modalActive"],
+    setup(props, { emit }) {
+      const close = () => {
+        emit("close");
+      };
+  
+      return { close };
+    },
+  };
+  </script>
--- assets/vue/controllers/Home.vue
+++ assets/vue/controllers/Home.vue
@@ -1,21 +1,56 @@
 <template>
     <div class="navbar">
-       <button class="about-us-btn">O NAMA</button>
-       <img src="/img/logo.png" alt="logo" class="logo">
-       <div class="login-signup">
-            <button class="login">PRIJAVI SE</button>
+        <button class="about-us-btn">O NAMA</button>
+        <img src="/img/logo.png" alt="logo" class="logo">
+        <div class="login-signup">
+            <button @click="toggleModal" type="button" class="login">PRIJAVI SE</button>
             <button class="signup">NAPRAVI NALOG</button>
-       </div>
+        </div>
     </div>
 
-    <div class="about-us-modal">
-            <h2>Pinpoint</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto necessitatibus accusantium voluptatem beatae natus aliquam quidem in sint ipsam voluptas. Aspernatur quia quibusdam ad autem totam minima dolor! Sequi, nemo!</p>
-        </div>
+    <div class="modal">
+        <Modal @close="toggleModal" :modalActive="modalActive">
+            <div class="modal-content">
+                <h1>This is a Modal Header</h1>
+                <p>This is a modal message</p>
+            </div>
+        </Modal>
+    </div>
+
+    <div class="middle-section">
+        <img src="/img/homepage-bg.jpg" class="middle-section-bg" alt="">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quod corporis ea accusantium assumenda aut
+        deleniti commodi maiores tempore possimus nisi sit minus aliquid corrupti fugit doloribus, aspernatur ratione ut!
+    </div>
 </template>
 
+<script>
+import Modal from "../components/Modal.vue";
+import { ref } from "vue";
+export default {
+    name: "modal",
+    components: {
+        Modal,
+    },
+    setup() {
+        const modalActive = ref(false);
+
+        const toggleModal = () => {
+            modalActive.value = !modalActive.value;
+        };
+
+        return { modalActive, toggleModal };
+    },
+};
+</script>
+
 <style scoped>
-.navbar{
+.modal-content {
+    display: flex;
+    flex-direction: column;
+}
+
+.navbar {
     width: 100%;
     position: sticky;
     background-color: #0f1e22;
@@ -23,9 +58,16 @@
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
+    top: 0;
 }
 
-.about-us-btn{
+.middle-section-bg {
+    width: 100%;
+    height: 800px;
+    object-fit: cover;
+}
+
+.about-us-btn {
     font-weight: 550;
     font-size: 15px;
     letter-spacing: -0.5px;
@@ -40,17 +82,17 @@
     transition: 0.2s ease-in-out;
 }
 
-.about-us-btn:hover{
+.about-us-btn:hover {
     background-color: #3d4142b0;
     border-radius: 5px;
     transition: 0.2s ease-in-out;
 }
 
-.about-us-modal{
+.about-us-modal {
     display: none;
 }
 
-.logo{
+.logo {
     color: white;
     font-size: 30px;
     width: 400px;
@@ -58,7 +100,11 @@
 
 }
 
-.login-signup button{
+.login-signup {
+    flex-shrink: 0;
+}
+
+.login-signup button {
     border-radius: 5px;
     outline: none;
     color: white;
@@ -69,25 +115,24 @@
     cursor: pointer;
 }
 
-.login{
+.login {
     background-color: #338ad7;
     border: none;
     transition: 0.2s ease-in-out;
 }
 
-.login:hover{
+.login:hover {
     background-color: #28669d;
     transition: 0.2s ease-in-out;
 }
 
-.signup{
+.signup {
     background-color: #b7b7b701;
     border: 1px solid white;
     transition: 0.2s ease-in-out;
 }
 
-.signup:hover{
+.signup:hover {
     background-color: #b7b7b777;
     transition: 0.2s ease-in-out;
-}
-</style>
+}</style>
