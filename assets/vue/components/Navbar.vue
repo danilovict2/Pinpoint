d8a823c5542543ff8a0955fd49519fb0e77caf86
Danilo <danilosindric@gmail.com>
2024-03-03 22:00:33 +0100

    Extract navbar to it's own component

---
+++ assets/vue/components/Navbar.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="navbar">
+        <button class="about-us-btn">O NAMA</button>
+        <img src="/img/logo.png" alt="logo" class="logo">
+        <div class="login-signup">
+            <button @click="emit('toggleLoginModal')" type="button" class="login">PRIJAVI SE</button>
+            <button @click="emit('toggleRegisterModal')" type="button" class="signup">NAPRAVI NALOG</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const emit = defineEmits(['toggleLoginModal', 'toggleRegisterModal']);
+
+</script>
+
+
+<style scoped>
+.navbar {
+    width: 100%;
+    position: sticky;
+    background-color: #0f1e22;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    top: 0;
+    box-shadow: 0px 5px 10px rgb(0, 0, 0);
+}
+
+.about-us-btn {
+    font-weight: 550;
+    font-size: 15px;
+    letter-spacing: -0.5px;
+    color: #9e9e9e;
+    background: none;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: 10px;
+    margin-left: 60px;
+    margin-right: 150px;
+    transition: 0.2s ease-in-out;
+}
+
+.about-us-btn:hover {
+    background-color: #3d4142b0;
+    border-radius: 5px;
+    transition: 0.2s ease-in-out;
+}
+
+.logo {
+    font-size: 30px;
+    width: 400px;
+    height: 60px;
+
+}
+
+.login-signup {
+    flex-shrink: 0;
+}
+
+.login-signup button {
+    border-radius: 5px;
+    outline: none;
+    color: white;
+    font-size: 18px;
+    padding: 8px;
+    font-weight: 600;
+    margin-right: 15px;
+    cursor: pointer;
+}
+
+.signup {
+    background-color: #b7b7b701;
+    border: 1px solid white;
+    transition: 0.2s ease-in-out;
+}
+
+.signup:hover {
+    background-color: #b7b7b777;
+    transition: 0.2s ease-in-out;
+}
+
+.login {
+    background-color: #338ad7;
+    border: none;
+    transition: 0.2s ease-in-out;
+}
+</style>
--- assets/vue/controllers/Home.vue
+++ assets/vue/controllers/Home.vue
@@ -1,12 +1,5 @@
 <template>
-    <div class="navbar">
-        <button class="about-us-btn">O NAMA</button>
-        <img src="/img/logo.png" alt="logo" class="logo">
-        <div class="login-signup">
-            <button @click="toggleLoginModal" type="button" class="login">PRIJAVI SE</button>
-            <button @click="toggleRegisterModal" type="button" class="signup">NAPRAVI NALOG</button>
-        </div>
-    </div>
+    <Navbar @toggle-login-modal="toggleLoginModal" @toggle-register-modal="toggleRegisterModal"></Navbar>
 
     <div class="middle-section">
         <img src="/img/homepage-bg.jpg" class="middle-section-bg" alt="">
@@ -22,6 +15,7 @@
 import { ref } from "vue";
 import LoginModal from "../components/LoginModal.vue";
 import RegisterModal from "../components/RegisterModal.vue";
+import Navbar from '../components/Navbar.vue';
 
 const loginModalActive = ref(false);
 const registerModalActive = ref(false);
@@ -36,18 +30,6 @@ function toggleRegisterModal() {
 </script>
 
 <style scoped>
-.navbar {
-    width: 100%;
-    position: sticky;
-    background-color: #0f1e22;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    top: 0;
-    box-shadow: 0px 5px 10px rgb(0, 0, 0);
-}
-
 .middle-section {
     position: relative;
 }
@@ -56,69 +38,5 @@ function toggleRegisterModal() {
     width: 100%;
     height: 800px;
     object-fit: cover;
-}
-
-.about-us-btn {
-    font-weight: 550;
-    font-size: 15px;
-    letter-spacing: -0.5px;
-    color: #9e9e9e;
-    background: none;
-    border: none;
-    outline: none;
-    cursor: pointer;
-    padding: 10px;
-    margin-left: 60px;
-    margin-right: 150px;
-    transition: 0.2s ease-in-out;
-}
-
-.about-us-btn:hover {
-    background-color: #3d4142b0;
-    border-radius: 5px;
-    transition: 0.2s ease-in-out;
-}
-
-.about-us-modal {
-    display: none;
-}
-
-.logo {
-    font-size: 30px;
-    width: 400px;
-    height: 60px;
-
-}
-
-.login-signup {
-    flex-shrink: 0;
-}
-
-.login-signup button {
-    border-radius: 5px;
-    outline: none;
-    color: white;
-    font-size: 18px;
-    padding: 8px;
-    font-weight: 600;
-    margin-right: 15px;
-    cursor: pointer;
-}
-
-.signup {
-    background-color: #b7b7b701;
-    border: 1px solid white;
-    transition: 0.2s ease-in-out;
-}
-
-.signup:hover {
-    background-color: #b7b7b777;
-    transition: 0.2s ease-in-out;
-}
-
-.login {
-    background-color: #338ad7;
-    border: none;
-    transition: 0.2s ease-in-out;
 }
 </style>
