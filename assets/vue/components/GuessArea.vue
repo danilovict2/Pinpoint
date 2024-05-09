1a2d9d8c225c10463795f47e764823072c71eeb7
Danilo <danilosindric@gmail.com>
2024-05-09 18:42:17 +0200

    Move map logic to Game.vue

--- assets/vue/components/GuessArea.vue
+++ assets/vue/components/GuessArea.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="guess-area">
-        <div class="map-area">
-            <Map @map-click="setCurrentMarker" @keyup.space="guessButton.click()"></Map>
-        </div>
+        <slot></slot>
         <div class="container">
             <div class="round-details">
                 <div class="box">
@@ -15,69 +13,27 @@
                     <h4>Vreme:<br> <span>{{ timer.minutes }}</span>:<span>{{ timer.seconds.value < 10 ? '0' : '' }}{{ timer.seconds }}</span></h4>
                 </div>
             </div>
-            <button :disabled="!isGuessable" @click="guess" ref="guessButton">Pogodi</button>
+            <button :disabled="!isGuessable" @click="emit('guessed')">Pogodi</button>
         </div>
     </div>
 </template>
 
 <script setup>
 import { onMounted, ref, watchEffect } from 'vue';
-import Map from './Map.vue';
 import { useTimer } from 'vue-timer-hook';
 const { startPosition } = defineProps({
-    startPosition: Object,
     round: Number
 });
 
 const emit = defineEmits(['guessed']);
-let currentMarker = null;
-const isGuessable = ref(false);
-const guessButton = ref(null);
+const isGuessable = ref(true);
 // FIXED TO THREE MINUTES
 const timer = useTimer(new Date().setSeconds(new Date().getSeconds() + 180));
-
-function setCurrentMarker(latLng, map) {
-    isGuessable.value = true;
-    currentMarker?.setMap(null);
-    currentMarker = new google.maps.Marker({
-        position: latLng,
-        map: map
-    });
-}
-
-function guess() {
-    // Earth's radius in metres (mean radius = 6,371km)
-    const radius = 6371e3;
-    const position = currentMarker.position;
-
-    // Angles need to be radians to pass trig functions!
-    const lat1Radian = (position.lat() * Math.PI) / 180;
-    const lat2Radian = (startPosition.lat * Math.PI) / 180;
-    const latDelta = ((position.lat() - startPosition.lat) * Math.PI) / 180;
-    const longDelta = ((position.lng() - startPosition.lng) * Math.PI) / 180;
-
-    // The square of half the chord length between the points
-    const a =
-        Math.sin(latDelta / 2) * Math.sin(latDelta / 2) +
-        Math.cos(lat1Radian) *
-        Math.cos(lat2Radian) *
-        Math.sin(longDelta / 2) *
-        Math.sin(longDelta / 2);
-
-    // Angular distance in radians
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    // Distance is the radius * angular distance
-    const distance = (radius * c) / 1000;
-    const score = Math.round(5000 * Math.exp(-0.5 * (distance / 2000) ** 2));
-    emit('guessed', score, { lat: position.lat(), lng: position.lng() }, distance);
-}
 
 onMounted(() => {   
     watchEffect(() => {
         if (timer.isExpired.value) {
-            if (currentMarker) guess();
-            else emit('guessed', 0, {}, -1);
+            emit('guessed');
         }
     });
 });
@@ -87,30 +43,6 @@ onMounted(() => {
 .guess-area {
     filter: invert(1);
     z-index: 10;
-}
-
-.map-area {
-    display: flex;
-    flex-direction: column;
-    width: 300px;
-    height: 250px;
-    gap: 10px;
-    position: absolute;
-    right: 3rem;
-    transition: all 0.1s ease-out;
-    bottom: 6rem;
-    opacity: .6;
-    transition-delay: 0.5s;
-    margin-top: 100px;
-}
-
-.map-area:hover {
-    width: 640px;
-    height: 700px;
-    margin-bottom: -150px;
-    opacity: 1;
-    transition-delay: 0s;
-    margin-top: 0px;
 }
 
 .container {
--- assets/vue/controllers/Game.vue
+++ assets/vue/controllers/Game.vue
@@ -3,9 +3,13 @@
     <div class="game" style="height: 100%;" v-else>
         <div class="standard-game" v-show="mode === 'STANDARD'" style="height: 100%;">
             <ThePanorama :start-position="currentRoundStartPosition" v-if="roundScore === null">
-                <TheGuessArea :start-position="currentRoundStartPosition" :round="round" @guessed="handleGuess">
-                </TheGuessArea>
+                <GuessArea :round="round" @guessed="calculateScore">
+                    <div class="map-area">
+                        <Map @map-click="setCurrentMarker" @keyup.space="calculateScore"></Map>
+                    </div>
+                </GuessArea>
             </ThePanorama>
+
             <TheRoundResult v-else :start-position="currentRoundStartPosition"
                 :guess-position="currentRoundGuessPosition" :score="roundScore" :round="round"
                 :distance="distanceFromGuess" @round-end="handleRoundEnd">
@@ -14,18 +18,19 @@
 
         <div class="flag-game" v-show="mode === 'FLAG'" style="height: 100%;">
             <TestMap></TestMap>
-            
+
         </div>
     </div>
 </template>
 
 <script setup>
 import TheFinalResults from '../components/TheFinalResults.vue';
-import TheGuessArea from '../components/TheGuessArea.vue';
+import GuessArea from '../components/GuessArea.vue';
 import ThePanorama from '../components/ThePanorama.vue';
 import TheRoundResult from '../components/TheRoundResult.vue';
 import { ref } from 'vue';
 import TestMap from './TestMap.vue';
+import Map from '../components/Map.vue';
 
 const { mode } = defineProps({
     mode: String
@@ -37,8 +42,15 @@ const currentRoundGuessPosition = ref(null);
 const round = ref(0);
 const isGameOver = ref(false);
 const distanceFromGuess = ref(0);
+let currentMarker = null;
 let score = 0;
 startRound();
+
+function startRound() {
+    currentRoundStartPosition.value = pickStartPosition();
+    roundScore.value = null;
+    round.value++;
+}
 
 function handleRoundEnd() {
     if (round.value < 5) {
@@ -48,10 +60,44 @@ function handleRoundEnd() {
     }
 }
 
-function startRound() {
-    currentRoundStartPosition.value = pickStartPosition();
-    roundScore.value = null;
-    round.value++;
+function setCurrentMarker(latLng, map) {
+    //isGuessable.value = true;
+    currentMarker?.setMap(null);
+    currentMarker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        icon: URL = "/img/player-marker.png"
+    });
+}
+
+function calculateScore() {
+    // Earth's radius in metres (mean radius = 6,371km)
+    const radius = 6371e3;
+    const position = currentMarker.position;
+    const startPosition = currentRoundStartPosition.value;
+
+    // Angles need to be radians to pass trig functions!
+    const lat1Radian = (position.lat() * Math.PI) / 180;
+    const lat2Radian = (startPosition.lat * Math.PI) / 180;
+    const latDelta = ((position.lat() - startPosition.lat) * Math.PI) / 180;
+    const longDelta = ((position.lng() - startPosition.lng) * Math.PI) / 180;
+
+    // The square of half the chord length between the points
+    const a =
+        Math.sin(latDelta / 2) * Math.sin(latDelta / 2) +
+        Math.cos(lat1Radian) *
+        Math.cos(lat2Radian) *
+        Math.sin(longDelta / 2) *
+        Math.sin(longDelta / 2);
+
+    // Angular distance in radians
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    // Distance is the radius * angular distance
+    distanceFromGuess.value = (radius * c) / 1000;
+    roundScore.value = Math.round(5000 * Math.exp(-0.5 * (distanceFromGuess.value / 2000) ** 2));
+    currentRoundGuessPosition.value = { lat: position.lat(), lng: position.lng() };
+    score += roundScore.value;
 }
 
 function pickStartPosition() {
@@ -109,13 +155,32 @@ function pickStartPosition() {
     return places[Math.floor(Math.random() * (places.length))];
 }
 
-function handleGuess(rndScore, guessPosition, distance) {
-    roundScore.value = rndScore;
-    currentRoundGuessPosition.value = guessPosition;
-    score += rndScore;
-    distanceFromGuess.value = distance;
-}
-
 // UNCOMMENT WHEN NOT IN DEVELOPMENT
 //window.onbeforeunload = () => "This page is asking you to confirm that you want to leave — information you've entered may not be saved."; 
 </script>
+
+<style scoped>
+.map-area {
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+    height: 250px;
+    gap: 10px;
+    position: absolute;
+    right: 3rem;
+    transition: all 0.1s ease-out;
+    bottom: 6rem;
+    opacity: .6;
+    transition-delay: 0.5s;
+    margin-top: 100px;
+}
+
+.map-area:hover {
+    width: 640px;
+    height: 700px;
+    margin-bottom: -150px;
+    opacity: 1;
+    transition-delay: 0s;
+    margin-top: 0px;
+}
+</style>
